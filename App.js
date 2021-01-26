760a2dcb12cb0064f2d4b5ce806a3c2583f26c93
Cesar Morales <cesarlenin2@live.com>
2021-01-26 15:07:47 -0800

    finished scroll list

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -16,7 +16,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-
+      <ScrollView>
+      { people.map(item=> (
+          <View key= {item.key}>
+            <Text style= {styles.item}>{item.name}</Text>
+          </View>
+        )
+      )}
+      </ScrollView>
     </View>
   );
 }
@@ -25,8 +32,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  item:{
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
 
 });
